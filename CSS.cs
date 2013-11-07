d4d4069d3dc420ced052bbab7fcf3a0057af9360
Kallikrates Koios <kallikrates.koios@gmail.com>
2013-11-07 17:56:18 -0500

    Began completely rewriting WebSharp.

--- CSS.cs
+++ CSS.cs
@@ -1,64 +1,102 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace WebSharp {
-	public struct Style{
-		public string Key;
-		public string Value;
-		public Style(string Key, string Value) {
-			this.Key = Key;
-			this.Value = Value;
+	public class PassonStylesheet {
+		protected Dictionary<Styleset, Tuple<string, HashSet<string>>> setDict;
+		public PassonStylesheet () {
+			setDict = new Dictionary<Styleset, Tuple<string, HashSet<string>>> ();
 		}
-		public static explicit operator KeyValuePair<string,string>(Style S) {
-			return new KeyValuePair<string, string> (S.Key, S.Value);
+		public string this[Styleset sset, string elementtype] {
+			get {
+				if (!setDict.ContainsKey(sset)) {
+					setDict [sset] = new Tuple<string, HashSet<string>> (setDict.Count.ToString("X") + sset.GetHashCode().ToString("X4"), new HashSet<string>());
+				}
+				setDict [sset].Item2.Add (elementtype);
+				return setDict [sset].Item1;
+			}
 		}
-		public static implicit operator Style(KeyValuePair<string,string> S) {
-			return new Style (S.Key, S.Value);
-		}
-		public override string ToString () {
-			return String.Format ("{0}:{1};", Key, Value);
+		public List<string> GetFormattedSheet () {
+			List<string> sheet = new List<string> ();
+			foreach (KeyValuePair<Styleset, Tuple<string, HashSet<string>>> KVP in setDict) {
+				foreach (string type in KVP.Value.Item2) {
+					sheet.Add (String.Format("{0}.{1} {{{2}}}", type, KVP.Value.Item1, KVP.Key.ToString()));
+				}
+			}
+			return sheet;
 		}
 	}
-
 	public class Styleset : Dictionary<string, string> {
-		public string Name;
-		public Styleset(string name, params Style[] styles) : base() {
-			Name = name;
-			foreach (Style S in styles) {
-				this [S.Key] = S.Value;
+		public Styleset(params Style[] styles) : base() {
+			foreach(Style S in styles) {
+				this [S.property] = S.value;
 			}
 		}
-		public override string ToString () {
-			string rstr = String.Empty;
+		public override int GetHashCode () {
+			string t = String.Empty;
 			foreach(Style S in this) {
-				rstr += S;
+				t += S.property;
 			}
-			return rstr;
+			return t.GetHashCode ();
 		}
-	}
-
-	public struct Styleclass {
-		public string Tag, Class, State;
-		public Styleclass (string Tag, string Class = "", string State = "") {
-			this.Tag = Tag;
-			this.Class = Class;
-			this.State = State;
+		public override bool Equals (object obj) {
+			if (obj == null)
+				return false;
+			Styleset S = obj as Styleset;
+			if (S == null)
+				return false;
+			if (S.Count != this.Count)
+				return false;
+			foreach(bool r in S.Zip(this, (a, b) => (a.Key == b.Key && a.Value == b.Value))) {
+				if (!r)
+					return false;
+			}
+			return true;
 		}
 		public override string ToString () {
-			return String.Format ("{0}{1}{2}", Tag, Class==""?"":"."+Class, State==""?"":":"+State);
+			return String.Join ("",this.Select((k, v) => String.Format("{0}:{1};", k, v)));
 		}
 	}
-
-	public class Stylesheet : Dictionary<Styleclass,Styleset> {
-		public Stylesheet() : base() {
-
+	public class Style : IComparable {
+		public string property;
+		public string value;
+		public Style (string property, string value) {
+			this.property = property;
+			this.value = value;
 		}
-
-		public override string ToString () {
-			string rstr = String.Empty;
-			foreach(KeyValuePair<Styleclass, Styleset> KVP in this) {
+		public override int GetHashCode () {
+			return (property).GetHashCode ();
+		}
+		public override bool Equals (object obj) {
+			if (obj == null)
+				return false;
+			Style S = obj as Style;
+			if (S == null)
+				return false;
+			if (this.property == S.property && this.value == S.value)
+				return true;
+			return false;
+		}
+		public int CompareTo(object obj) {
+			if (obj == null)
+				return 1;
+			Style S = obj as Style;
+			if (S == null)
+				return 1;
+			int ps = this.property.CompareTo (S.property);
+			if (ps == 0){
+				return this.value.CompareTo (S.value);
+			} else {
+				return ps;
 			}
-			return rstr;
+		}
+		public override string ToString () {
+			return String.Format ("{0}:{1};", this.property, this.value);
+		}
+		public static implicit operator Style(KeyValuePair<string, string> KVP) {
+			return new Style (KVP.Key, KVP.Value);
 		}
 	}
 }
--- HTML.cs
+++ HTML.cs
@@ -1,11 +1,113 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WebSharp {
-	public class HTMLAttributes : Dictionary<string,string> {
+	//BEGIN INTERFACE
+	public interface IElement {
+		string Type { get;}
+		string WOToString (PassonStylesheet PS); //Web Optimized ToString()
+	}
+	//END INTERFACE BEGIN ATTRIBUTESET
+	public class AttributeSet {
+		protected Dictionary<string, string> attributes = new Dictionary<string, string>();
+		protected Styleset styleset;
+		protected List<Style> additionalStyles = new List<Style>();
+		public AttributeSet() {
+
+		}
+		public string GetPassonText(PassonStylesheet PS, string elementtype) {
+			return (styleset != null ? String.Format(" class=\"{0}\"", PS[styleset, elementtype]) : String.Empty) + (additionalStyles.Count > 0 ? String.Format(" style=\"{0}\"", String.Join("", additionalStyles)) : String.Empty) + String.Join ("", attributes.Select((k, v) => String.Format(" {0}=\"{1}\"", k, v)));
+		}
+		public override string ToString () {
+			return (additionalStyles.Count > 0 ? String.Format(" style=\"{0}\"", String.Join("", additionalStyles) + String.Join("", styleset.Select((k, v) => String.Format("{0}:{1};", k, v)))) : String.Empty) + String.Join ("", attributes.Select((k, v) => String.Format(" {0}=\"{1}\"", k, v)));
+		}
+	}
+	//END ATTRIBUTESET BEGIN ABSTRACTS
+	public abstract class ElementEmpty : IElement {
+		protected string name;
+		public AttributeSet attributes = new AttributeSet();
+		public string Type { get{return name;} }
+		public ElementEmpty (string name) {
+			this.name = name;
+		}
+		public virtual string WOToString(PassonStylesheet PS) {
+			return String.Format ("<{0}{1}>", this.Type, this.attributes.GetPassonText(PS, name));
+		}
+		public override string ToString () {
+			return String.Format ("<{0}{1}>", this.Type, this.attributes.ToString());
+		}
+	}
+	public abstract class ElementText : IElement {
+		protected string name;
+		public AttributeSet attributes = new AttributeSet();
+		public string Text;
+		public string Type { get{return name;} }
+		public ElementText (string name, string text) {
+			this.name = name;
+			this.Text = text;
+		}
+		public virtual string WOToString(PassonStylesheet PS) {
+			return String.Format ("<{0}{1}>{2}<{0}>", this.Type, this.attributes.GetPassonText(PS, name), this.Text);
+		}
+		public override string ToString () {
+			return String.Format ("<{0}{1}>{2}<{0}>", this.Type, this.attributes.ToString(), this.Text);
+		}
+	}
+	public abstract class ElementContainer : List<IElement>, IElement {
+		protected string name;
+		public AttributeSet attributes = new AttributeSet();
+		public string Type { get{return name;} }
+		public ElementContainer (string name, params IElement[] contents) : base (contents) {
+			this.name = name;
+		}
+		public virtual string WOToString(PassonStylesheet PS) {
+			return String.Format ("<{0}{1}>{2}<{0}>", this.Type, this.attributes.GetPassonText(PS, name), String.Join("", this.Select((e) => e.WOToString(PS))));
+		}
+		public override string ToString () {
+			return String.Format ("<{0}{1}>{2}<{0}>", this.Type, this.attributes.ToString(), String.Join("", this));
+		}
+	}
+	//END ABSTRACTS BEGIN EMPTIES
+	public class Breakline : ElementEmpty {
+		public Breakline () : base ("br") {
+
+		}
+	}
+	//END EMPTIES BEGIN TEXTS
+	public class Span : ElementText {
+		public Span (string text) : base ("span", text) {
+
+		}
+	}
+	//END TEXTS BEGIN CONTAINERS
+	public class Divider : ElementContainer {
+		public Divider (params IElement[] contents) : base ("div", contents) {
+
+		}
+	}
+	//END CONTAINERS BEGIN STRUCTURES
+	//END STRUCTURES
+	/*public class HTMLAttributes : Dictionary<string,string> {
+		internal Styleset styleSet;
+		internal List<Style> stylesAdditional = new List<Style>();
 		public HTMLAttributes() : base() {}
 		internal HTMLAttributes Attrib(string key, string value) {
 			this [key] = value;
+			return this;
+		}
+		public HTMLAttributes AddStyle(Style S) {
+			stylesAdditional.Add (S);
+			return this;
+		}
+		public HTMLAttributes AddStyles(params Style[] SS) {
+			foreach(Style S in SS) {
+				stylesAdditional.Add (S);
+			}
+			return this;
+		}
+		public HTMLAttributes SetStyleset(Styleset S) {
+			this.styleSet = S;
 			return this;
 		}
 		public HTMLAttributes Action(string value) {return Attrib ("action", value);}
@@ -16,13 +118,42 @@ namespace WebSharp {
 		public HTMLAttributes Reference(string value) {return Attrib ("href", value);}
 		public HTMLAttributes Relationship(string value) {return Attrib ("rel", value);}
 		public HTMLAttributes Source(string value) {return Attrib ("src", value);}
-		public HTMLAttributes Style(string value) {return Attrib ("style", value);}
+		//public HTMLAttributes Style(string value) {return Attrib ("style", value);}
 		public HTMLAttributes Title(string value) {return Attrib ("title", value);}
 		public HTMLAttributes Type(string value) {return Attrib ("type", value);}
 		public HTMLAttributes Value(string value) {return Attrib ("value", value);}
 
+		internal string ToStringWebOp (string classname) {
+			string rstr = String.Format(" class=\"{0}\"", classname);
+			string sstr = String.Empty;
+			if (this.stylesAdditional.Count > 0) {
+				foreach (Style S in this.stylesAdditional) {
+					sstr += S;
+				}
+			}
+			if (sstr != String.Empty)
+				sstr = String.Format (" style=\"{0}\"", sstr);
+			rstr += sstr;
+			foreach (KeyValuePair<string,string> KVP in this) {
+				rstr += String.Format (" {0}=\"{1}\"", KVP.Key, KVP.Value);
+			}
+			return rstr;
+		}
+
 		public override string ToString () {
 			string rstr = String.Empty;
+			string sstr = String.Empty;
+			if (this.styleSet != null) {
+				sstr += styleSet;
+			}
+			if (this.stylesAdditional.Count > 0) {
+				foreach (Style S in this.stylesAdditional) {
+					sstr += S;
+				}
+			}
+			if (sstr != String.Empty)
+				sstr = String.Format (" style=\"{0}\"", sstr);
+			rstr += sstr;
 			foreach (KeyValuePair<string,string> KVP in this) {
 				rstr += String.Format (" {0}=\"{1}\"", KVP.Key, KVP.Value);
 			}
@@ -33,6 +164,7 @@ namespace WebSharp {
 	public interface IElement {
 		string Name { get; }
 		HTMLAttributes Attributes { get; }
+		string ToStringWebOp (string classname);
 	}
 	public class ElementEmpty : IElement {
 		private string name;
@@ -42,6 +174,9 @@ namespace WebSharp {
 		public ElementEmpty(string name) {
 			this.name = name;
 			attrib = new HTMLAttributes ();
+		}
+		public string ToStringWebOp(string classname) {
+			return String.Format ("<{0}{1}>", Name, Attributes.ToStringWebOp(classname));
 		}
 		public override string ToString () {
 			return String.Format ("<{0}{1}>", Name, Attributes);
@@ -58,6 +193,11 @@ namespace WebSharp {
 			this.Content = content;
 			attrib = new HTMLAttributes ();
 		}
+		public string ToStringWebOp(string classname) {
+			string fcont = Content.Replace("\r\n", "<br>");
+			fcont = fcont.Replace("\n", "<br>");
+			return String.Format ("<{0}{1}>{2}</{0}>", Name, Attributes.ToStringWebOp(classname), fcont);
+		}
 		public override string ToString () {
 			string fcont = Content.Replace("\r\n", "<br>");
 			fcont = fcont.Replace("\n", "<br>");
@@ -73,6 +213,13 @@ namespace WebSharp {
 			this.name = name;
 			attrib = new HTMLAttributes ();
 		}
+		public string ToStringWebOp(string classname) {
+			string contents = String.Empty;
+			foreach (IElement E in this) {
+				contents += E;
+			}
+			return String.Format ("<{0}{1}>{2}</{0}>", Name, Attributes.ToStringWebOp(classname), contents);
+		}
 		public override string ToString () {
 			string contents = String.Empty;
 			foreach (IElement E in this) {
@@ -82,8 +229,14 @@ namespace WebSharp {
 		}
 	}
 
+	public class Webpage {
+		public Webpage () {
+
+		}
+	}
+
 	public static class HTML {
 		public static readonly ElementEmpty Breakline = new ElementEmpty ("br");
-	}
+	} */
 }
 
